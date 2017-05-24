700efcb2287418ccb37ad8056d33c98b0c833a82
calvin-li <calli@ucdavis.edu>
2017-05-24 00:00:04 -0700

    Started on main UI, integrating sample ExpandableListView

---
+++ app/src/main/java/com/sandbox/calvin_li/quest/ExpandableListAdapter/ExpandableListAdapter.kt
@@ -0,0 +1,76 @@
+package com.sandbox.calvin_li.quest.ExpandableListAdapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Typeface
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+import com.sandbox.calvin_li.quest.R
+
+import java.util.HashMap
+
+class ExpandableListAdapter(
+        private val _context: Context,
+        private val _listDataHeader: List<String>,
+        private val _listDataChild: HashMap<String, List<String>>)
+    : BaseExpandableListAdapter() {
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any? {
+        return this._listDataChild[this._listDataHeader[groupPosition]]?.get(childPosition)
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun getChildView(
+            groupPosition: Int,
+            childPosition: Int,
+            isLastChild: Boolean,
+            convertView: View?,
+            parent: ViewGroup)
+            : View {
+        val childText: String = getChild(groupPosition, childPosition) as String
+
+        val newConvertView: View = convertView ?: (this._context.getSystemService(Context
+                .LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.element, null)
+
+        val textListChild: TextView =
+                (convertView ?: newConvertView).findViewById(R.id.element) as TextView
+
+        textListChild.text = childText
+        return convertView ?: newConvertView
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int =
+            this._listDataChild[this._listDataHeader[groupPosition]]?.size as Int
+
+    override fun getGroup(groupPosition: Int): Any? = this._listDataHeader[groupPosition]
+
+    override fun getGroupCount(): Int = this._listDataHeader.size
+
+    override fun getGroupId(groupPosition: Int): Long = groupPosition.toLong()
+
+    override fun getGroupView(
+            groupPosition: Int,
+            isExpanded: Boolean,
+            convertView: View?,
+            parent: ViewGroup
+    ): View? {
+        val returnedView: View = convertView ?: (this._context.getSystemService(Context
+                .LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.element, null)
+
+        val labelListHeader: TextView = convertView?.findViewById(R.id.element) as TextView
+        labelListHeader.setTypeface(null, Typeface.BOLD)
+        labelListHeader.text = getGroup(groupPosition) as String
+
+        return returnedView
+    }
+
+    override fun hasStableIds(): Boolean = false
+
+    override fun isChildSelectable(i: Int, i1: Int): Boolean = true
+}
