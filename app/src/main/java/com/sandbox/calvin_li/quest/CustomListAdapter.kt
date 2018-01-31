4389bd56440136f8cf5eb101abcd72ec275f2362
calvin-li <calli@ucdavis.edu>
2018-01-31 00:33:11 -0800

    got collapsing to work basically.'

--- app/src/main/java/com/sandbox/calvin_li/quest/CustomListAdapter.kt
+++ app/src/main/java/com/sandbox/calvin_li/quest/CustomListAdapter.kt
@@ -1,8 +1,6 @@
 package com.sandbox.calvin_li.quest
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,9 +11,9 @@ import com.beust.klaxon.JsonObject
 class CustomListAdapter(
         context: Context,
         private var quests: Array<Quest> =
-            flatten(MainActivity.questJson, emptyList()).toTypedArray())
+            flatten(MainActivity.questJson, emptyList()).toTypedArray()
+        )
     : ArrayAdapter<Quest>(context, R.layout.element_dialog, quests) {
-
     internal companion object {
         private const val indentSize = 24 // in dp
 
@@ -25,7 +23,8 @@ class CustomListAdapter(
                 listOf(Quest(
                     jsonObject[Quest.nameLabel] as String,
                     currentIndex.plus(index),
-                    jsonObject[Quest.expandLabel] as Boolean))
+                    jsonObject[Quest.expandLabel] as Boolean,
+                    isHidden(currentIndex)))
                         .plus(
                                 @Suppress("UNCHECKED_CAST")
                                 flatten(
@@ -34,10 +33,18 @@ class CustomListAdapter(
                         )
             }?.flatten() ?: emptyList()
         }
+
+        private fun isHidden(index: List<Int>) =
+            (index.size - 1 downTo 0).toList()
+                .mapIndexed{ it, _ -> index.subList(0, it+1) }
+                .any { !(MainActivity.getNestedArray(it)[Quest.expandLabel] as Boolean) }
     }
 
+    private var visibleQuests = quests.filter { !it.hidden }
+        get() = quests.filter { !it.hidden }
+
     internal val onItemClickListener = AdapterView.OnItemClickListener {
-        _: AdapterView<*>?, _: View?, position: Int, _: Long ->
+        adapterView: AdapterView<*>?, _: View?, position: Int, _: Long ->
         MainActivity.loadQuestJson(this.context)
 
         val quest = getItem(position)
@@ -45,7 +52,9 @@ class CustomListAdapter(
 
         MainActivity.saveJson(this.context)
         notifyDataSetChanged()
-    }
+
+        (adapterView as ListView).setSelection(position)
+}
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val currentQuest = getItem(position)
@@ -63,13 +72,10 @@ class CustomListAdapter(
 
         val questView = container.findViewById(R.id.element_header_text) as TextView
 
-        val isHidden = checkIfHidden(currentQuest.index)
-
-        MainActivity.loadQuestJson(this.context)
-        if(MainActivity.getNestedArray(currentQuest.index)[Quest.expandLabel] as Boolean) {
-            questView.setTextColor(Color.rgb(16,128,16))
+        if(currentQuest.hidden) {
+            container.visibility = View.GONE
         } else{
-            questView.setTextColor(this.context.resources.getColor(R.color.colorPrimaryDark))
+            container.visibility = View.VISIBLE
         }
 
         questView.text = currentQuest.name
@@ -86,29 +92,21 @@ class CustomListAdapter(
         return container
     }
 
-    private fun checkIfHidden(index: List<Int>): Boolean {
-
-        val intProgression = (index.size - 1 downTo 0).toList()
-        return intProgression
-            .map { it -> index.subList(0, it) }
-            .any { !(MainActivity.getNestedArray(it)[Quest.expandLabel] as Boolean) }
-    }
-
     override fun notifyDataSetChanged() {
         MainActivity.loadQuestJson(this.context)
         quests = flatten(MainActivity.questJson, emptyList()).toTypedArray()
         super.notifyDataSetChanged()
     }
 
-    override fun getCount(): Int = quests.count()
+    override fun getCount(): Int = visibleQuests.count()
 
-    override fun getItem(position: Int): Quest = quests[position]
+    override fun getItem(position: Int): Quest = visibleQuests[position]
 
     override fun getItemId(position: Int): Long {
         val currentQuest = getItem(position)
-        return currentQuest.name.hashCode().toLong() * currentQuest.index.hashCode() //implicit cast
-        // right operand
+        return currentQuest.name.hashCode().toLong() * currentQuest.index.hashCode()
+        //implicit cast right operand
     }
 
-    override fun getPosition(item: Quest?): Int = quests.indexOfFirst { it == item}
+    override fun getPosition(item: Quest?): Int = visibleQuests.indexOfFirst {it == item}
 }
--- app/src/main/java/com/sandbox/calvin_li/quest/Quest.kt
+++ app/src/main/java/com/sandbox/calvin_li/quest/Quest.kt
@@ -3,7 +3,8 @@ package com.sandbox.calvin_li.quest
 class Quest(
     internal val name: String,
     internal val index: List<Int>,
-    internal val expanded: Boolean
+    internal val expanded: Boolean,
+    internal val hidden: Boolean
 ){
     internal companion object {
         internal const val nameLabel: String = "name"
